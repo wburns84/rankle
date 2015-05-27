58df26923f6adf434b77c1dd634adffef3c4eb78
Wil <github@william-burns.com>
2015-05-27 09:37:19 -0700

    Added setup and teardown hooks for database cleaner

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,6 @@ require 'minitest/pride'
 require 'rankle'
 require 'database_cleaner'
 
-DatabaseCleaner.strategy = :truncation
-
 ActiveRecord::Base.establish_connection(
     adapter:  'sqlite3',
     database: 'rankle.sqlite3'
@@ -12,3 +10,15 @@ ActiveRecord::Base.establish_connection(
 
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/models.rb'
+
+DatabaseCleaner.strategy = :truncation
+
+class Minitest::Test
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+end

ba64efe9db461acde7b4256f5f14f6e894432e19
Wil <github@william-burns.com>
2015-05-27 09:37:41 -0700

    Added remaining default behavior tests

--- test/default_behavior_test.rb
+++ test/default_behavior_test.rb
@@ -1,7 +1,22 @@
 require_relative './test_helper'
 
-class TestMeme < Minitest::Test
-  def test_that_rankle_is_ineffectual
+class TestDefaultBehavior < Minitest::Test
+  def test_it_is_ineffectual
     assert(Fruit.all.to_a == Fruit.ranked.to_a)
+  end
+
+  def test_it_sets_position
+    apple  = Fruit.create!
+    orange = Fruit.create!
+
+    assert_equal 0, apple.position
+    assert_equal 1, orange.position
+  end
+
+  def test_it_ranks_on_insert_order
+    Fruit.create! name: 'apple'
+    Fruit.create! name: 'orange'
+
+    assert_equal ['apple', 'orange'], Fruit.ranked.map(&:name)
   end
 end

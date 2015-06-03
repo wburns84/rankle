650d59e1b81e792ab456e871e0973e9eb5de01a0
Wil <github@william-burns.com>
2015-06-03 10:05:50 -0700

    Added benchmark for reverse insert

--- test/performance/rank_benchmark.rb
+++ test/performance/rank_benchmark.rb
@@ -3,17 +3,37 @@ require 'minitest/benchmark'
 
 class BenchmarkRank < Minitest::Benchmark
   def self.bench_range
-    Minitest::Benchmark.bench_exp 8, 256, 2
+    Minitest::Benchmark.bench_exp 16, 256, 2
   end
+
+  def trials
+    1
+  end
+
 
   def bench_insert_has_linear_performance
     assert_performance_linear 0.99 do |n|
-      1.times do
+      trials.times do
         DatabaseCleaner.clean
         n.times { |name| Fruit.create! name: name }
       end
     end
 
+    DatabaseCleaner.clean
+  end
+
+  def bench_reverse_insert_has_power_performance
+    Fruit.send :ranks, ->(a, b) { a.name.to_i > b.name.to_i }
+
+    assert_performance_power 0.99 do |n|
+      trials.times do
+        DatabaseCleaner.clean
+        n.times { |name| Fruit.create! name: name }
+      end
+    end
+
+    # FIXME: This unfortunate hack reaches into the internals of RankleIndex to reset the test state
+    RankleIndex.instance_variable_set(:@rankers, {})
     DatabaseCleaner.clean
   end
 end

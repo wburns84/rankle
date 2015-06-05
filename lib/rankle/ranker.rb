e9130566dd0084bd1b991720786f6e25be5c19b8
Wil <github@william-burns.com>
2015-06-05 07:57:37 -0700

    Added relative insert for second element

--- lib/rankle/ranker.rb
+++ lib/rankle/ranker.rb
@@ -10,7 +10,13 @@ module Rankle
     end
 
     def self.insert target_position, existing_elements
-      return 0, existing_elements
+      if existing_elements.empty?
+        return 0, []
+      elsif target_position <= 0
+        return (MIN_INDEX + existing_elements.first) / 2, existing_elements
+      else
+        return (MAX_INDEX + existing_elements.last) / 2, existing_elements
+      end
     end
   end
 end
--- test/spec/ranker_test.rb
+++ test/spec/ranker_test.rb
@@ -21,5 +21,25 @@ describe Rankle::Ranker do
         end
       end
     end
+
+    describe 'singleton array' do
+      describe 'at position 0' do
+        it 'returns -1073741824' do
+          assert_equal [-1073741824, [0]], Rankle::Ranker.insert(0, [0])
+        end
+      end
+
+      describe 'at position -100' do
+        it 'returns -1073741824' do
+          assert_equal [-1073741824, [0]], Rankle::Ranker.insert(-100, [0])
+        end
+      end
+
+      describe 'at position 100' do
+        it 'returns 1073741823' do
+          assert_equal [1073741823, [0]], Rankle::Ranker.insert(100, [0])
+        end
+      end
+    end
   end
 end

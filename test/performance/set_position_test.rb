a128b6c9790483c77a313cae3611e0b644a44405
Wil <github@william-burns.com>
2015-06-17 21:05:17 -0700

    Assert set postion query count

---
+++ test/performance/set_position_test.rb
@@ -0,0 +1,17 @@
+require_relative '../support/test_helper'
+
+class TestSetPosition < Minitest::Test
+  def test_initializing_position_makes_six_queries
+    assert_queries(6) do
+      apple  = Fruit.create! name: 'apple'
+    end
+  end
+
+  def test_update_position_makes_three_queries
+    apple  = Fruit.create! name: 'apple'
+
+    assert_queries(3) do
+      apple.update_attribute :position, 1
+    end
+  end
+end

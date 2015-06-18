aaf52d878ebd9dc5a9fe682794fd54805aec5c04
Wil <github@william-burns.com>
2015-06-17 20:58:37 -0700

    Added assert_queries

--- test/support/test_helper.rb
+++ test/support/test_helper.rb
@@ -6,8 +6,8 @@ require 'rails/generators'
 require 'rake'
 
 ActiveRecord::Base.establish_connection(
-    adapter:  'sqlite3',
-    database: 'rankle.sqlite3'
+  adapter:  'sqlite3',
+  database: 'rankle.sqlite3'
 )
 
 rake = Rake.application
@@ -32,4 +32,15 @@ class Minitest::Test
   def setup
     DatabaseCleaner.clean
   end
+end
+
+def assert_queries(num = 1, &block)
+  queries  = []
+  callback = lambda { |name, start, finish, id, payload|
+    queries << payload[:sql] if payload[:sql] =~ /^SELECT|UPDATE|INSERT/
+  }
+
+  ActiveSupport::Notifications.subscribed(callback, "sql.active_record", &block)
+ensure
+  assert_equal num, queries.size, "#{queries.size} instead of #{num} queries were executed.#{queries.size == 0 ? '' : "\nQueries:\n#{queries.join("\n")}"}"
 end
